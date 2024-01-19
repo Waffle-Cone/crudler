599f4a4f8c3050cf5e5dd477c2ea1468fa96794b
Winter <ldbtci2000@gmail.com>
2024-01-19 12:45:44 +0000

    W14A4
    DONE

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,51 +1,26 @@
-import {Pressable, ScrollView, StyleSheet,Text, View } from 'react-native';
+import {StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
-
 import initialModules from '../../data/modules.js'
+import ModuleList from '../entity/modules/ModuleList';
 
 const ModuleListScreen = () => {
   // Initialisations ---------------------
-  const modules = initialModules;
+  const modules = initialModules; // module data
+
   // State -------------------------------
   // Handlers ----------------------------
- const handleSelect = ()=>{alert("Item Selected")};
+ const handleSelect = (module)=>{alert(`Item ${module.ModuleCode} Selected`)};
   // View --------------------------------
 
   return (
       <Screen>  
-        <ScrollView style= {styles.container}>
-        {
-          modules.map((module) => {
-            return(
-              <Pressable key={module.ModuleCode} onPress ={handleSelect}>
-                <View style= {styles.item}>
-                  <Text style= {styles.text}>{module.ModuleCode} {module.ModuleName}</Text>
-                </View> 
-              </Pressable>
-              
-
-            )
-          })
-        }
-        </ScrollView>
+        <ModuleList modules={modules} onSelect={handleSelect}/>
       </Screen> 
       
       
   );
 }
 
-const styles = StyleSheet.create({
-  container: {},
-
-  item: {
-    paddingVertical:15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-
-  text:{
-    ontSize:16,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default ModuleListScreen;
